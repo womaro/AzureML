d8a9b00aa0c3145e218205b494a8b6f2a7e3c98f
Wojtek <wojtek.rogowiec@gmail.com>
2025-04-24 18:24:32 +0200

    Simple regression in jupyter add

--- AzureML.py
+++ AzureML.py
@@ -1,1 +1,7 @@
-import tensorflow as tf
+# Import necessary libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
